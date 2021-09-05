8dacab679c59441cf885de63b80750fef878ff9d
João <jmambrini@gmail.com>
2021-09-05 16:00:43 -0300

    Criação da pagina inicial

---
+++ src/components/pagInicial/pagInicial.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const pagInicial = () => <div>Bem vindos aos site do TCC do João</div>;
+
+export default pagInicial;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -7,11 +7,17 @@ import PublicRoutes from './PublicRoutes';
 
 import Teste from '../components/Teste/Teste';
 import Topico2 from '../components/Topico2/Tópico2';
+import pagInicial from '../components/pagInicial/pagInicial';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <PublicRoutes
+          path="/"
+          exact
+          component={pagInicial}
+        />
         <PublicRoutes
           path="/intro"
           exact
